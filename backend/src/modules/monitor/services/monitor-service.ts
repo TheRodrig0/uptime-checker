348cd85297b460d11089f32fb2729c3135058dc8
TheRodrig0 <rodrigogeronimo11@gmail.com>
2025-06-30 10:05:43 -0300

    refactor: update monitor service to improve error handling and code readability

--- backend/src/modules/monitor/services/monitor-service.ts
+++ backend/src/modules/monitor/services/monitor-service.ts
@@ -1,21 +1,24 @@
 import type { CrudServiceInterface } from "../../types/crud-service-interface"
-import type { MonitorRequestDTO, MonitorReplyDTO } from "../types"
+import type { MonitorRequestDTO, MonitorReplyDTO } from "../types/dtos"
 import type { CrudRepositoryInterface } from "../../types/crud-repository-interface"
 import { MonitorEntity } from "../entities/monitor-entity"
 import { ID } from "../../value-objects/id"
+import { NotFoundError } from "../../../utils/error/not-found-error"
 
 export class MonitorService implements CrudServiceInterface<MonitorRequestDTO, MonitorReplyDTO> {
     constructor(private readonly repository: CrudRepositoryInterface<MonitorEntity, ID>) { }
 
     async findAll(): Promise<MonitorReplyDTO[]> {
         const monitors = await this.repository.findAll()
+
         return monitors.map(monitor => monitor.toJSON())
     }
 
     async findById(id: string): Promise<MonitorReplyDTO | null> {
         const monitorId = new ID(id)
         const monitor = await this.repository.findById(monitorId)
-        return monitor ? monitor.toJSON() : null
+
+        return monitor?.toJSON() ?? null
     }
 
     async create(data: MonitorRequestDTO): Promise<MonitorReplyDTO> {
@@ -30,12 +33,14 @@ export class MonitorService implements CrudServiceInterface<MonitorRequestDTO, M
         const monitor = await this.repository.findById(monitorId)
 
         if (!monitor) {
-            throw new Error(`Monitor has not found for update.`)
+            throw new NotFoundError(`Monitor doesn't found for update`)
         }
+
+        monitor.update(data)
 
         const updatedMonitor = await this.repository.update(monitorId, monitor)
 
-        return updatedMonitor ? updatedMonitor.toJSON() : null
+        return updatedMonitor?.toJSON() ?? null
     }
 
     async delete(id: string): Promise<void> {
@@ -43,7 +48,7 @@ export class MonitorService implements CrudServiceInterface<MonitorRequestDTO, M
         const monitor = await this.repository.findById(monitorId)
 
         if (!monitor) {
-            throw new Error(`Monitor has not found for deletion.`)
+            throw new Error(`Monitor doesn't found for deletion`)
         }
 
         await this.repository.delete(monitorId)
