d596044c8717a5fe8a3e7e963f63de2a8c3ab674
TheRodrig0 <rodrigogeronimo11@gmail.com>
2025-07-15 17:01:20 -0300

    feat: added inversify type for dependency injection

---
+++ backend/src/modules/monitor/types/inversify-types.ts
@@ -0,0 +1,5 @@
+export const TYPES = {
+    MonitorController: Symbol.for("MonitorController"),
+    MonitorService: Symbol.for("MonitorService"),
+    InMemoryMonitorRepository: Symbol.for("InMemoryMonitorRepository"),
+}
