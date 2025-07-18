52a2a7f62c0255b4212ce0492c6b59ff7bfad968
TheRodrig0 <rodrigogeronimo11@gmail.com>
2025-07-18 16:55:46 -0300

    feat: enhance Title value object with regex validation and improve error messages

--- backend/src/modules/monitor/value-objects/title-value-object.ts
+++ backend/src/modules/monitor/value-objects/title-value-object.ts
@@ -2,18 +2,27 @@ import { BaseValueObject } from "../../shared/value-objects/base-value-object"
 import { BadRequestError } from "../../shared/custom-errors/bad-request-error"
 
 export class Title extends BaseValueObject<string> {
-    private readonly minimumLength: number = 5
-    private readonly maximumLength: number = 10
-    
+    private static readonly minimumLength: number = 5
+    private static readonly maximumLength: number = 10
+
     protected validate(value: string): void {
+        this.validateWithRegex(value)
         this.validateLength(value)
     }
 
-    private validateLength(value: string) {
-        const isValidLength = value.length >= this.minimumLength && value.length <= this.maximumLength
+    private validateWithRegex(value: string) {
+        const titleRegex = /^(?!.*\p{Extended_Pictographic})[\p{L}\p{N}\s.,!?'"-]+$/u
 
-        if(!isValidLength){
-            throw new BadRequestError("")
+        if (!titleRegex.test(value)) {
+            throw new BadRequestError("Invalid title. It may contain invalid characters or emojis.")
+        }
+    }
+
+    private validateLength(value: string) {
+        const isValidLength = value.length >= Title.minimumLength && value.length <= Title.maximumLength
+
+        if (!isValidLength) {
+            throw new BadRequestError(`Invalid title length, title isn't between ${Title.minimumLength} and ${Title.maximumLength} in length`)
         }
     }
 }
