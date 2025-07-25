432c3ee5dc82b0523ce33641c7a4582fa5447735
TheRodrig0 <rodrigogeronimo11@gmail.com>
2025-07-25 15:31:34 -0300

    refactor: restore findAll methods in CRUD interfaces for consistency

--- backend/src/modules/shared/types/crud-interfaces.ts
+++ backend/src/modules/shared/types/crud-interfaces.ts
@@ -1,34 +1,34 @@
 import type { IRequest, IReply } from "../../../types/common/http-interfaces"
 
 export interface IBaseCrudController<CreateDTO> {
-    findAll(request: IRequest): Promise<IReply>
     findOne(request: IRequest<unknown, { id: string }>): Promise<IReply>
     create(request: IRequest<CreateDTO>): Promise<IReply>
 }
 
 export interface IBaseCrudService<CreateDTO, ReplyDTO, UniqueFilter> {
-    findAll(where?: Partial<ReplyDTO>): Promise<ReplyDTO[]>
     findOne(filter: UniqueFilter): Promise<ReplyDTO | null>
     create(data: CreateDTO): Promise<ReplyDTO>
 }
 
 export interface IBaseCrudRepository<Entity, UniqueFilter> {
-    findAll(): Promise<Entity[]>
     findOne(filter: UniqueFilter): Promise<Entity | null>
     create(entity: Entity): Promise<Entity>
 }
 
 export interface ICrudController<CreateDTO, UpdateDTO> extends IBaseCrudController<CreateDTO> {
+    findAll(request: IRequest): Promise<IReply>
     update(request: IRequest<UpdateDTO, { id: string }>): Promise<IReply>
     delete(request: IRequest<unknown, { id: string }>): Promise<IReply>
 }
 
 export interface ICrudService<CreateDTO, UpdateDTO, ReplyDTO, UniqueFilter> extends IBaseCrudService<CreateDTO, ReplyDTO, UniqueFilter> {
+    findAll(where?: Partial<ReplyDTO>): Promise<ReplyDTO[]>
     update(filter: UniqueFilter, data: UpdateDTO): Promise<ReplyDTO | null>
     delete(filter: UniqueFilter): Promise<void>
 }
 
 export interface ICrudRepository<Entity, UniqueFilter> extends IBaseCrudRepository<Entity, UniqueFilter> {
+    findAll(): Promise<Entity[]>
     update(filter: UniqueFilter, entity: Entity): Promise<Entity | null>
     delete(filter: UniqueFilter): Promise<boolean>
 }
